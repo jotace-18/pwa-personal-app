1356b54a550f2efb8f9c5550565843fb3417e580
jotace-18 <jucamonro02@gmail.com>
2025-03-04 08:32:31 +0100

    Haciendo el login todavia

--- frontend/src/features/auth/AuthPage.tsx
+++ frontend/src/features/auth/AuthPage.tsx
@@ -1,16 +1,19 @@
+import LoginForm from "./LoginForm";
+
+
 const AuthPage = () => {
     return (
       <div className="vh-100 w-100 bg-light d-flex justify-content-center align-items-center">
-        <div className="bg-secondary p-5 rounded position-relative" style={{ width: "90%", height: "90%", opacity: 0.8 }}>
+        <div className="bg-secondary p-5 rounded position-relative" style={{ width: "90%", height: "95%", opacity: 0.8 }}>
           
           {/* Contenedor oscuro */}
           <div className="bg-dark rounded p-5 d-flex justify-content-center align-items-center position-relative"
                style={{ width: "65%", height: "100%" }}>
             
             {/* Tarjeta del login */}
-            <div className="bg-light rounded p-4 shadow-lg d-flex flex-column justify-content-center"
-                 style={{ width: "60%", height: "95%" }}> {/* Ajustamos la altura */}
-              <h2 className="text-center text-dark">LOGIN</h2>
+            <div className="bg-light rounded p-4 shadow-lg d-flex flex-column align-content-center"
+                 style={{ width: "50%", height: "100%" }}> {/* Ajustamos la altura */}
+                 < LoginForm />
             </div>
   
             {/* Logo en la esquina superior izquierda del contenedor oscuro */}
--- frontend/src/features/auth/LoginForm.tsx
+++ frontend/src/features/auth/LoginForm.tsx
@@ -1,9 +1,43 @@
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
 const LoginForm = () => {
-    return (
-      <form>
-        {/* Aquí irán los campos del formulario */}
-      </form>
-    );
+  return (
+    <form>
+    <div className="text-center mb-3 py-4" style={{ fontSize: '32px' }}>Login Form</div>
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+      <span className="input-group-text"><FaUser style={{fontSize: '24px'}}/></span>
+      </div>
+      <input type="text" className="form-control" placeholder="Nombre de usuario" />
+    </div>
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <span className="input-group-text"><FaLock style={{ fontSize: '24px' }} /></span>
+      </div>
+      <input type="password" className="form-control" placeholder="Contraseña" />
+    </div>
+    <div className="form-check mb-3">
+      <input type="checkbox" className="form-check-input" id="Recuérdame" />
+      <label className="form-check-label" htmlFor="Recuérdame">Recuérdame</label>
+
+    </div>
+    <div className="mb-3 text-end">
+      <a href="#">¿Olvidaste tu contraseña?</a>
+    </div>
+    <div className="d-grid">
+      <button type="submit" className="btn btn-secondary text-black">Iniciar sesión</button>
+      
+    </div>
+    <div className="mb-3 text-center">
+      <p>o</p>
+    </div>
+    <div className="mb-3 text-center">
+      <p>Registrate <a href="#">aqui</a></p>
+    </div>
+    
+    </form>
+  );
   };
   
   export default LoginForm;
